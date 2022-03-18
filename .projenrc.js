b4d83d87e1ca06f368c3b3afc54faa8253828b05
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-03-18 12:45:54 +0100

    chore: switch over to my base

--- .projenrc.js
+++ .projenrc.js
@@ -1,14 +1,20 @@
-const { cdktf } = require('projen');
-const project = new cdktf.ConstructLibraryCdktf({
-  author: 'Daniel Schmidt',
-  authorAddress: 'danielmschmidt92@gmail.com',
-  cdktfVersion: '0.4.0',
-  defaultReleaseBranch: 'main',
-  name: 'cdktf-tf-module-stack',
-  repositoryUrl: 'https://github.com/danielmschmidt92/cdktf-tf-module-stack.git',
+const { CDKTFConstruct } = require("@dschmidt/cdktf-construct-base");
+const project = new CDKTFConstruct({
+  author: "Daniel Schmidt",
+  authorAddress: "danielmschmidt92@gmail.com",
+  defaultReleaseBranch: "main",
+  name: "cdktf-tf-module-stack",
+  repositoryUrl: "https://github.com/DanielMSchmidt/cdktf-tf-module-stack.git",
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  deps: [],
+  peerDeps: ["cdktf"],
+  devDeps: [
+    "@cdktf/provider-null",
+    "@cdktf/provider-random",
+    "@dschmidt/cdktf-construct-base",
+  ],
+  description:
+    "A drop-in replacement for cdktf.TerraformStack that let's you define Terraform modules as construct" /* The description is just a string that helps people understand the purpose of the package. */,
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
   // release: undefined,      /* Add release management to this project. */
