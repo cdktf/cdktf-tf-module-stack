e820b8cc5e8583bf8634f4fc4b1b3f22bc249deb
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-03-23 18:09:18 +0100

    feat: also remove backends

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { NullProvider, Resource } from "@cdktf/provider-null";
-import { Testing } from "cdktf";
+import { RemoteBackend, Testing } from "cdktf";
 import { Construct } from "constructs";
 import { TFModuleStack } from "../src";
 
@@ -9,6 +9,41 @@ test("synthesizes a provider", () => {
     constructor(scope: Construct, id: string) {
       super(scope, id);
 
+      new NullProvider(this, "null");
+      new Resource(this, "resource");
+    }
+  }
+  const stack = new MyStack(app, "MyStack");
+  expect(Testing.synth(stack)).toMatchInlineSnapshot(`
+    "{
+      \\"resource\\": {
+        \\"null_resource\\": {
+          \\"resource\\": {
+          }
+        }
+      },
+      \\"terraform\\": {
+        \\"required_providers\\": {
+          \\"null\\": {
+            \\"source\\": \\"null\\",
+            \\"version\\": \\"~> 2.0\\"
+          }
+        }
+      }
+    }"
+  `);
+});
+
+test("synthesizes no backend", () => {
+  const app = Testing.app();
+  class MyStack extends TFModuleStack {
+    constructor(scope: Construct, id: string) {
+      super(scope, id);
+
+      new RemoteBackend(this, {
+        organization: "test",
+        workspaces: [{ name: "test" }],
+      });
       new NullProvider(this, "null");
       new Resource(this, "resource");
     }
