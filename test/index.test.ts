7fca1ee5b7bef9cd26393266c180d41e73480b45
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-05-06 13:40:14 +0200

    feat: dont require providers since they pin versions now
    and allow users to specify provider requirements

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,15 +2,77 @@ import { NullProvider, Resource } from "@cdktf/provider-null";
 import { RemoteBackend, Testing } from "cdktf";
 import { Construct } from "constructs";
 import { TFModuleStack } from "../src";
-import { TFModuleVariable } from "../src/index";
+import { TFModuleVariable, ProviderRequirement } from "../src/index";
 
-test("synthesizes a provider", () => {
+test("synthesizes a specific provider", () => {
   const app = Testing.app();
   class MyStack extends TFModuleStack {
     constructor(scope: Construct, id: string) {
       super(scope, id);
 
       new NullProvider(this, "null");
+      new Resource(this, "resource");
+    }
+  }
+  const stack = new MyStack(app, "MyStack");
+  expect(Testing.synth(stack)).toMatchInlineSnapshot(`
+    "{
+      \\"resource\\": {
+        \\"null_resource\\": {
+          \\"resource\\": {
+          }
+        }
+      },
+      \\"terraform\\": {
+        \\"required_providers\\": {
+          \\"null\\": {
+            \\"source\\": \\"null\\",
+            \\"version\\": \\"2.1.2\\"
+          }
+        }
+      }
+    }"
+  `);
+});
+
+test("synthesizes an abstract provider", () => {
+  const app = Testing.app();
+  class MyStack extends TFModuleStack {
+    constructor(scope: Construct, id: string) {
+      super(scope, id);
+
+      new ProviderRequirement(this, "null", ">= 2.0.0");
+      new Resource(this, "resource");
+    }
+  }
+  const stack = new MyStack(app, "MyStack");
+  expect(Testing.synth(stack)).toMatchInlineSnapshot(`
+    "{
+      \\"resource\\": {
+        \\"null_resource\\": {
+          \\"resource\\": {
+          }
+        }
+      },
+      \\"terraform\\": {
+        \\"required_providers\\": {
+          \\"null\\": {
+            \\"source\\": \\"null\\",
+            \\"version\\": \\"2.1.2\\"
+          }
+        }
+      }
+    }"
+  `);
+});
+
+test("synthesizes a provider with a specific source", () => {
+  const app = Testing.app();
+  class MyStack extends TFModuleStack {
+    constructor(scope: Construct, id: string) {
+      super(scope, id);
+
+      new ProviderRequirement(this, "null", ">= 2.0.0", "hashicorp/null");
       new Resource(this, "resource");
     }
   }
