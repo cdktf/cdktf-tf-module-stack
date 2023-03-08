2d7514cc38409c027c5170189bf938f1b335dc31
Ansgar Mertens <ansgar@hashicorp.com>
2023-03-08 10:19:38 +0100

    chore: configure projen to run copywrite after build on PRs

--- .projenrc.ts
+++ .projenrc.ts
@@ -149,4 +149,15 @@ new JsiiDocgen(project, {
 
 project.jest?.addIgnorePattern("dist");
 project.addKeywords("cdktf", "terraform");
+
+// Run copywrite tool to add copyright headers to all files
+project.buildWorkflow?.addPostBuildSteps(
+  {
+    name: "Setup Copywrite tool",
+    uses: "hashicorp/setup-copywrite@3ace06ad72e6ec679ea8572457b17dbc3960b8ce", // v1.0.0
+    with: { token: "${{ secrets.GITHUB_TOKEN }}" },
+  },
+  { name: "Add headers using Copywrite tool", run: "copywrite headers" }
+);
+
 project.synth();
