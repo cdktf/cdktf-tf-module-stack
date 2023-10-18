1e0bd92c14f6575eca6ab287980b2ad8b2d53e94
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-10-18 09:41:27 +0200

    chore: update peer dependencies

--- .projenrc.ts
+++ .projenrc.ts
@@ -80,8 +80,8 @@ new UpgradeCDKTF(project);
 
 project.addPeerDeps("cdktf@>=0.19.0", "constructs@^10.0.25");
 project.addDevDeps(
-  "@cdktf/provider-null@>=8.0.0",
-  "@cdktf/provider-random@>=9.0.0"
+  "@cdktf/provider-null@>=9.0.0",
+  "@cdktf/provider-random@>=10.0.0"
 );
 
 new JsiiDocgen(project, {
