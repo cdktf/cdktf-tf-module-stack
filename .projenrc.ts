be4fd1f5c4d3cdb6a665474bdcf9ee1ea576780d
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2024-07-08 16:03:43 +0200

    chore(deps): upgrade jsii & typescript to v5.4 (#325)

--- .projenrc.ts
+++ .projenrc.ts
@@ -71,8 +71,8 @@ const project = new ConstructLibraryCdktf({
     packageName: "tfmodulestack",
   },
   docgen: false,
-  jsiiVersion: "~5.2.0",
-  typescriptVersion: "~5.2.0", // should always be the same major/minor as JSII
+  jsiiVersion: "~5.4.0",
+  typescriptVersion: "~5.4.0", // should always be the same major/minor as JSII
 });
 
 new CustomizedLicense(project);
